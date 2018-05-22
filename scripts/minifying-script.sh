9ea5039c7894294c166012d2a0d23780b77c0705
Yaser Alkayale <technoligest@gmail.com>
2018-05-22 19:10:03 -0300

    Miified html, and smaller images

---
+++ scripts/minifying-script.sh
@@ -0,0 +1,1 @@
+html-minifier -no-html5 --output --collapse-whitespace --minify-css --remove-comments --process-scripts --sort-attributes --collapse-boolean-attributes --decode-entities --minify-ur-ls --no-include-auto-generated-tags --trim-custom-fragments  --collapse-inline-tag-whitespace --remove-tag-whitespace -o minified.html index.html
