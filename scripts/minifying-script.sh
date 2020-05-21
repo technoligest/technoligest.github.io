f7f82976d6ec98fb140a5a98458e1095893a5265
Yaser Alkayale <technoligest@gmail.com>
2020-05-21 03:35:30 -0700

    FB new website design

--- scripts/minifying-script.sh
+++
@@ -1,1 +0,0 @@
-html-minifier -no-html5 --output --collapse-whitespace --minify-css --remove-comments --process-scripts --sort-attributes --collapse-boolean-attributes --decode-entities --minify-ur-ls --no-include-auto-generated-tags --trim-custom-fragments  --collapse-inline-tag-whitespace --remove-tag-whitespace -o index.html index-original.html
