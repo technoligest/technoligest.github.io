2cf2bb34758f4558590a96e44e72c9e31e7ef5e2
Yaser Alkayale <technoligest@gmail.com>
2024-08-10 10:38:51 -0700

    save

--- docs/_next/static/chunks/app/page-2b7a9298c62a880b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{9775:function(e,n,s){Promise.resolve().then(s.t.bind(s,8173,23)),Promise.resolve().then(s.t.bind(s,231,23)),Promise.resolve().then(s.t.bind(s,7797,23))}},function(e){e.O(0,[307,329,971,23,744],function(){return e(e.s=9775)}),_N_E=e.O()}]);
